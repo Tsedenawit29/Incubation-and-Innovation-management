cf6be995ed93a19c5159f80795a60f858e75129e
unknown <redietberhanu2@gmail.com>
2025-07-15 14:58:56 +0300

    Application and ApplicationForm entity created

---
+++ iims/src/main/java/com/iims/iims/application_from/entity/Application.java
@@ -0,0 +1,56 @@
+package com.iims.iims.application_from.entity;
+
+import com.iims.iims.tenant.entity.Tenant;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "applications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Application {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column (name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "form_id", nullable = false)
+    private ApplicationForm form;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tenant_id", nullable = false)
+    private Tenant tenant;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "firat_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "applicant_type", nullable = false)
+    private ApplicantType applicantType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ApplicationStatus status = ApplicationStatus.PENDING;
+
+    @Column(name = "submitted_at", updatable = false, nullable = false)
+    private LocalDateTime submittedAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "application", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ApplicationResponse> responses;
+}
