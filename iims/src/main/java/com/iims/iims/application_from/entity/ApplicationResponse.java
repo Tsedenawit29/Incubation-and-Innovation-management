785ff330637bba659cac4c836708a1eaeed26d82
unknown <redietberhanu2@gmail.com>
2025-07-17 14:05:30 +0300

    ApplicationResponse entity created

---
+++ iims/src/main/java/com/iims/iims/application_from/entity/ApplicationResponse.java
@@ -0,0 +1,35 @@
+package com.iims.iims.application_from.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "application_responses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationResponse {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_id", nullable = false)
+    private Application application;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "field_id", nullable = false)
+    private ApplicationFormField field;
+
+    @Column(name = "response")
+    private String response;
+}
