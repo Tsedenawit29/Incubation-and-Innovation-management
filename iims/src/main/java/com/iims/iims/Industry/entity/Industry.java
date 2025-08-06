f0c897750e217c68afce76fe6c605297fac50f7a
unknown <redietberhanu2@gmail.com>
2025-08-06 16:24:26 +0300

    correctly done the service and controler of industy

--- iims/src/main/java/com/iims/iims/Industry/entity/Industry.java
+++ iims/src/main/java/com/iims/iims/Industry/entity/Industry.java
@@ -28,4 +28,36 @@ public class Industry {
     private String name;
 
     private String description;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
