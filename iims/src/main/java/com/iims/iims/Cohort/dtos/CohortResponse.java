0467700391c7c3e8b5e48af8fea214b1eba50846
unknown <redietberhanu2@gmail.com>
2025-08-06 15:37:08 +0300

    cohort service and controler done

--- iims/src/main/java/com/iims/iims/Cohort/dtos/CohortResponse.java
+++ iims/src/main/java/com/iims/iims/Cohort/dtos/CohortResponse.java
@@ -1,18 +1,81 @@
 package com.iims.iims.Cohort.dtos;
 
 import com.iims.iims.tenant.entity.Tenant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CohortResponse {
 
     private UUID id;
-    private Tenant tenantId;
+    private UUID tenantId;
     private String name;
     private String description;
     private Boolean isActive;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(UUID tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
     private LocalDate startDate;
     private LocalDate endDate;
 }
